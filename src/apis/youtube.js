91d2cb21ee2bb0077e638f87bd07a71e48e88e8c
simsalabim08 <westra77@gmail.com>
2021-01-06 23:07:32 +0100

    Committing project

---
+++ src/apis/youtube.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const KEY = 'AddKeyHere';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        type: 'video',
+        maxResults: 5,
+        key: KEY
+    }
+});
+
